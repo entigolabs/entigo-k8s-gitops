6c8c6d347dd3c9c32f0f622be021573986d691af
Märt Erlenheim <mart.erlenheim@entigo.com>
2021-06-03 10:08:22 +0300

    Catch and log SIGINT/SIGTERM for diagnostics

--- cmd/gitops/main.go
+++ cmd/gitops/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"errors"
 	"github.com/entigolabs/entigo-k8s-gitops/internal/app/gitops/cli"
+	"github.com/entigolabs/entigo-k8s-gitops/internal/app/gitops/common"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	cli.Run()
+	terminated := make(chan os.Signal, 1)
+	signal.Notify(terminated, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		cli.Run()
+	}()
+
+	<-terminated
+	common.Logger.Println(&common.Warning{Reason: errors.New("utility was terminated, exiting")})
 }
