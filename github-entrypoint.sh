a8c81e5b21f64a5db80bba7fd1a4d21b478c8b24
martivo <martin.vool@entigo.com>
2021-12-21 17:51:48 +0200

    Fix key

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "-----BEGIN RSA PRIVATE KEY-----" > /key-file
+
+
+echo "$INPUT_GIT_KEY" > /key-file
 chmod 600 /key-file
-echo $INPUT_GIT_KEY >> /key-file
-echo "-----END RSA PRIVATE KEY-----"  >> /key-file
+cat /key-file | sed 's/./& /g'
 
 gitops update \
     --git-repo=$INPUT_GIT_REPO \
