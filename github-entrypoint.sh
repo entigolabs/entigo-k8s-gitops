856d9af0f114ff15bff6dfd8c29a5f8924bae95e
martivo <martin.vool@entigo.com>
2021-12-21 18:02:58 +0200

    Write key to shm

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-
 tocuh /dev/shm/key-file && chmod 600 /dev/shm/key-file && echo "$INPUT_GIT_KEY" > /dev/shm/key-file && \
 gitops update \
-    --git-repo=$INPUT_GIT_REPO \
-    --git-branch=$INPUT_GIT_BRANCH \
-    --git-key-file=/dev/shm/key-file \
-    --images=$INPUT_IMAGES \
-    --app-path=$INPUT_APP_PATH
+    --git-repo="$INPUT_GIT_REPO" \
+    --git-branch="$INPUT_GIT_BRANCH" \
+    --git-key-file="/dev/shm/key-file" \
+    --images="$INPUT_IMAGES" \
+    --app-path="$INPUT_APP_PATH"
 
