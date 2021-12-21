4574994252b3b3b1c7ce363c497c4eec735efac7
martivo <martin.vool@entigo.com>
2021-12-21 18:29:05 +0200

    Add support for more commands

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-tocuh /dev/shm/key-file && chmod 600 /dev/shm/key-file && echo "$INPUT_GIT_KEY" > /dev/shm/key-file && \
+touch /dev/shm/key-file && chmod 600 /dev/shm/key-file && echo "$INPUT_GIT_KEY" > /dev/shm/key-file
+
+if [ "$INPUT_COMMAND" == "update" ]
+then
 gitops update \
     --git-repo="$INPUT_GIT_REPO" \
     --git-branch="$INPUT_GIT_BRANCH" \
@@ -7,3 +10,16 @@ gitops update \
     --images="$INPUT_IMAGES" \
     --app-path="$INPUT_APP_PATH"
 
+elif [ "$INPUT_COMMAND" == "argocd-update" ]
+then
+gitops argocd-update \
+    --git-key-file="/dev/shm/key-file" \
+    --images="$INPUT_IMAGES" \
+    --app-name="${INPUT_APP_NAME}" \
+    --server="${INPUT_ARGOCD_SERVER}" \
+    --auth-token="${INPUT_ARGOCD_TOKEN}"
+
+else
+  echo "Unknown command: $INPUT_COMMAND"
+  exit 100
+fi
